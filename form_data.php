a64babda6fca58303cf9efb5c75a65cdc7cab536
unknown <hlogsatyr@gmail.com>
2021-08-02 15:37:18 +0300

    coomit

---
+++ form_data.php
@@ -0,0 +1,17 @@
+<?php
+function add_msg($name, $message){
+	$file = fopen('chat.txt', 'a');
+		$message = [
+			'name' => $name,
+			'message' => $message.= date("\ y.m.d.\ H:m:s."),
+		];
+		fwrite($file, json_encode($message) . PHP_EOL); 
+		fclose($file);
+		
+	}
+$message = trim($_POST['msg'] ?? null);
+$name = trim($_POST['name'] ?? null);
+if(!empty($name) && !empty($message)) {
+	add_msg($name, $message);
+	}
+header('Location: /index.php');
---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<meta charset="utf-u8">
+	</head>
+		<title>
+			Otpravka Form
+		</title>
+			<body>
+			<form action="./form_data.php" enctype="multipart/form-data" method="post">
+			 Login
+			 <input name="name" type="text" maxlength="40" size="45" Value="Name">
+			 <div>Инфо</div>
+			 <div><textarea name="msg"></textarea></div>
+			 <div><input type="submit" name="knpopka"></div>
+			 Chat
+			 <div>
+				<?php
+					$file = fopen('chat.txt', 'r');
+					while($chatline = fgets($file)) {
+						$chat = json_decode($chatline, true);
+				?>
+					<div class="message">
+						<div class="login"><?php echo $chat['name'];?></div>
+						<div class="text"><?php echo $chat['message'];?></div>
+					</div>
+				<?php
+					}
+					fclose($file);
+				?>
+			 </div>		 
+</html>
