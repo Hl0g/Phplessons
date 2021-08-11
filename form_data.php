f7484e36a5871bcd7a9c4a5b40c3b449dc3076df
unknown <hlogsatyr@gmail.com>
2021-08-11 14:36:11 +0300

    git

--- form_data.php
+++ form_data.php
@@ -1,9 +1,8 @@
 <?php
-require_once "./Functions.php";
+require_once __DIR__ .'/functions.php';
 $filename = 'chat.txt.';
 $name = trim($_POST['name'] ?? null);
 $message = trim($_POST['msg'] ?? null);
-
 if(!empty($name) && !empty($message)){
 	add_msg($name, $message, $filename);	
 }
